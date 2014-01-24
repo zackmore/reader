eb6ffc510af59b0429df129c60c9ba09a23e5c9d
zackmore <zackmoredev@gmail.com>
2014-01-24 18:52:26 +0800

    feed fetches and dump to database

--- feedfetcher.py
+++ feedfetcher.py
@@ -1,1 +1,53 @@
+# -*- coding: utf-8 -*-
+
+import os.path
 import requests
+import feedparser
+import hashlib
+import model
+
+
+import pdb
+
+
+class Dumper(object):
+    def __init__(self, feed_url):
+        self.data = feedparser.parse(feed_url)
+
+    def dump_feed(self):
+        self.feed = model.Feed(
+                feedname=self.data.feed.title.encode('utf-8'),
+                feedurl=self.data.feed.link.encode('utf-8'),
+                feedpubdate=self.data.feed.get('published_parsed',
+                                                'updated_parsed').encode('utf-8')
+                )
+
+    def dump_items(self):
+        for entry in self.data.entries:
+            url = entry.link
+            #pubdate = entry.get('published_parsed', 'updated_parsed')
+            pubdate = entry.get('published', 'updated')
+            title = entry.title
+            snippet = entry.get('summary', 'description')
+            content = entry.get('content', 'description')
+            guid = hashlib.md5(title+url).hexdigest()
+
+
+            item = model.Item(
+                        url=url.encode('utf-8'),
+                        pubdate=pubdate.encode('utf-8'),
+                        title=title.encode('utf-8'),
+                        snippet=snippet.encode('utf-8'),
+                        content=content.encode('utf-8'),
+                        guid=guid.encode('utf-8')
+                    )
+            self.feed.items.append(item)
+
+    def save_to_db(self):
+        model.session.add(self.feed)
+        model.session.commit()
+
+
+if __name__ == '__main__':
+    dumper = Dumper('./testfeed.xml')
+    pdb.set_trace()
--- model.py
+++ model.py
@@ -1,1 +1,65 @@
-import sqlalchemy
+import pdb
+import os.path
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship, backref
+
+Base = declarative_base()
+
+
+class Admin(Base):
+    __tablename__ = 'admin'
+
+    userid = Column(Integer, primary_key=True)
+    username = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+
+    def __repr__(self):
+        return '<Admin(username="%s")' % self.username
+
+
+class Feed(Base):
+    __tablename__ = 'feeds'
+
+    feedid = Column(Integer, primary_key=True)
+    feedname = Column(String, nullable=False)
+    feedurl = Column(String, nullable=False)
+    feedpubdate = Column(String)
+    itemunread = Column(Integer)
+
+    def __repr__(self):
+        return '<Feed(feed="%s", url="%s")>' % (self.feedname,
+                                                self.feedurl)
+
+
+class Item(Base):
+    __tablename__ = 'items'
+
+    itemid = Column(Integer, primary_key=True)
+    feedid = Column(Integer, ForeignKey('feeds.feedid'))
+    feed = relationship('Feed', backref=backref('items',
+                                                order_by=itemid))
+    url = Column(String)
+    pubdate = Column(String)
+    title = Column(String, nullable=False)
+    snippet = Column(String)
+    content = Column(String)
+    readed = Column(Boolean, default=False)
+    star = Column(Boolean, default=False)
+    guid = Column(String)
+
+    def __repr__(self):
+        return '<Item(title="%s", feed="%s")' % (self.title,
+                                                self.feed.feedname)
+
+
+db_file_path = 'sqlite:///' +\
+                os.path.dirname(__file__) +\
+                '/db/data.sqlite'
+engine = create_engine(db_file_path)
+
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
